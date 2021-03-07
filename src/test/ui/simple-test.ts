0f495c5fc1658dc21892e9edc02fe62803078710
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2021-03-07 00:19:07 -0500

    Test on multiple OSs, fix test

--- src/test/ui/simple-test.ts
+++ src/test/ui/simple-test.ts
@@ -15,6 +15,6 @@ describe('My Test Suite', () => {
     // test whatever we want using webdriver, here we are just checking the page title
     it('My Test Case', async () => {
         const title = await driver.getTitle();
-        assert.equal(title, 'Welcome');
+        assert.include(title, 'Welcome');
     });
 });
