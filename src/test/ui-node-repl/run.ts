644a2c840485e405940028a8b448e8913d0a2894
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2021-03-07 19:19:23 -0500

    Toward interactive environment for developing UI tests
    `run.ts` also runs in `ts-node`:
    
    ```sh
    ts-node
    ```
    
    and enter
    
    ```ts-node
    .load src/test/ui-node-repl/run.ts
    ```

---
+++ src/test/ui-node-repl/run.ts
@@ -0,0 +1,27 @@
+import { ExTester, ReleaseQuality } from 'vscode-extension-tester'
+
+// copied from vscode-extension-tester/src/cli.ts
+function codeStream(stream: string) {
+    const envType = process.env.CODE_TYPE
+    let type = stream
+
+    if (!type && envType) {
+        type = envType
+    }
+    if (type && type.toLowerCase() === 'insider') {
+        return ReleaseQuality.Insider
+    }
+    return ReleaseQuality.Stable
+}
+
+const vscode_release = codeStream(undefined)
+const storage_dir = './ui-test-resources'
+const extensions_dir = undefined
+const extest = new ExTester(storage_dir, vscode_release, extensions_dir)
+const testFiles = 'out/src/test/ui/*.js'
+const run_options = { // `: RunOptions` , but that is not exported.
+    vscodeVersion: undefined,
+    settings: undefined,
+}
+
+extest.runTests(testFiles, run_options)
