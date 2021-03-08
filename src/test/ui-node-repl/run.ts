5ca7d120662f638a4c336256783d91635dabf966
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2021-03-08 08:25:00 -0500

    A self-contained example of controlling VS Code UI
    Avoids using
    https://mochajs.org/api/mocha#addFile
    
    to make it easier to understand and debug and ideally use within a REPL
    
    FYI copied code at revision:
    https://github.com/redhat-developer/vscode-extension-tester/tree/ad612fd34c645d1973c8775c342a10defeea4795

--- src/test/ui-node-repl/run.ts
+++ src/test/ui-node-repl/run.ts
@@ -1,10 +1,7 @@
-import * as fs from 'fs-extra'
-import * as glob from 'glob'
 import * as path from 'path'
-import { ExTester, ReleaseQuality, VSBrowser } from 'vscode-extension-tester'
+import { ExTester, ReleaseQuality, VSBrowser, Workbench } from 'vscode-extension-tester'
 import { VSRunner } from 'vscode-extension-tester/out/suite/runner'
 import { DEFAULT_RUN_OPTIONS } from 'vscode-extension-tester/out/util/codeUtil'
-import sanitize = require('sanitize-filename');
 
 // copied from vscode-extension-tester/src/cli.ts
 function codeStream(stream: string) {
@@ -24,7 +21,6 @@ const vscode_release = codeStream(undefined)
 const storage_dir = './ui-test-resources'
 const extensions_dir = undefined
 const extest = new ExTester(storage_dir, vscode_release, extensions_dir)
-const testFilesPattern = 'out/src/test/ui/*.js'
 const runOptions = { // `: RunOptions` , but that is not exported.
     vscodeVersion: undefined,
     settings: undefined,
@@ -51,56 +47,49 @@ async function doit() {
     process.env.TEST_RESOURCES = this_.downloadFolder
     process.env.EXTENSIONS_FOLDER = this_.extensionsFolder
     const runner = new VSRunner(this_.executablePath, literalVersion, this_.parseSettings(runOptions.settings ?? DEFAULT_RUN_OPTIONS.settings), runOptions.cleanup, runOptions.config)
-    const code = this_
+
     const logLevel = runOptions.logLevel // The logging level for the Webdriver
     // body of `VSRunner.runTests` in `src/suite/runner.ts`
     const this__ = (<any>runner) // escape hatch to access private members
-    return new Promise(resolve => {
-        const self = this__
-        const browser: VSBrowser = new VSBrowser(this__.codeVersion, this__.customSettings, logLevel)
-        const universalPattern = testFilesPattern.replace(/'/g, '')
-        const testFiles = glob.sync(universalPattern)
 
-        testFiles.forEach((file) => {
-            if (fs.existsSync(file) && file.substr(-3) === '.js') {
-                this__.mocha.addFile(file)
-            }
-        })
+    const self = this__
+    const browser: VSBrowser = new VSBrowser(this__.codeVersion, this__.customSettings, logLevel)
 
-        this__.mocha.suite.afterEach(async function () {
-            if (this__.currentTest && this__.currentTest.state !== 'passed') {
-                try {
-                    const filename = sanitize(this__.currentTest.fullTitle())
-                    await browser.takeScreenshot(filename)
-                } catch (err) {
-                    console.log('Screenshot capture failed.', err)
-                }
-            }
-        })
+    // this__.mocha.suite.beforeAll
+    const start = Date.now()
+    await browser.start(self.chromeBin)
+    await browser.waitForWorkbench()
+    await new Promise((res) => { setTimeout(res, 2000) })
+    console.log(`Browser ready in ${Date.now() - start} ms`)
+    console.log('Launching tests...')
 
-        this__.mocha.suite.beforeAll(async function () {
-            this.timeout(45000)
-            const start = Date.now()
-            await browser.start(self.chromeBin)
-            await browser.waitForWorkbench()
-            await new Promise((res) => { setTimeout(res, 2000) })
-            console.log(`Browser ready in ${Date.now() - start} ms`)
-            console.log('Launching tests...')
-        })
+    // testing code here
+    //browser = VSBrowser.instance
 
-        this__.mocha.suite.afterAll(async function () {
-            this.timeout(15000)
-            await browser.quit()
+    browser.takeScreenshot('initialized')
 
-            code.uninstallExtension(self.cleanup)
-        })
+    //view = new ActivityBar().getViewControl('Extensions')
+    //sideBar = await view.openView()
+    //quickBox = await new Workbench().openCommandPrompt()
+    const workbench = new Workbench()
+    await workbench.executeCommand('Julia Start REPL')
+    await browser.takeScreenshot('start-repl-1')
+    await new Promise(r => setTimeout(r, 1000))
+    await browser.takeScreenshot('start-repl-2')
+    await new Promise(r => setTimeout(r, 4000))
+    await browser.takeScreenshot('start-repl-3')
+    await new Promise(r => setTimeout(r, 4000))
+    await browser.takeScreenshot('start-repl-4')
 
-        this__.mocha.run((failures) => {
-            process.exitCode = failures ? 1 : 0
-            resolve(process.exitCode)
-        })
-    })
+    // this__.mocha.suite.afterAll
+    await browser.quit()
 
+    /*
+    causesExtension 'vscode-extension-tester.api-handler' is not installed.
+
+    Error: Command failed: ELECTRON_RUN_AS_NODE=1 "/Users/goretkin/projects/LoggingPlayground/julia-vscode/ui-test-resources/Visual Studio Code.app/Contents/MacOS/Electron" "/Users/goretkin/projects/LoggingPlayground/julia-vscode/ui-test-resources/Visual Studio Code.app/Contents/Resources/app/out/cli.js" --uninstall-extension "vscode-extension-tester.api-handler"
+    */
+    //code.uninstallExtension(self.cleanup)
 }
 
 doit() // top-level await is supported in node in e.g. `.mjs` files, but just wrap it here, because typescript etc.
