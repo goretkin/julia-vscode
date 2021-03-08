cbaca7465affd1c2637c6f9c853bbabf7dfeaefa
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2021-03-08 08:08:34 -0500

    Peel indirection

--- src/test/ui-node-repl/run.ts
+++ src/test/ui-node-repl/run.ts
@@ -1,7 +1,10 @@
+import * as fs from 'fs-extra'
+import * as glob from 'glob'
 import * as path from 'path'
-import { ExTester, ReleaseQuality } from 'vscode-extension-tester'
+import { ExTester, ReleaseQuality, VSBrowser } from 'vscode-extension-tester'
 import { VSRunner } from 'vscode-extension-tester/out/suite/runner'
 import { DEFAULT_RUN_OPTIONS } from 'vscode-extension-tester/out/util/codeUtil'
+import sanitize = require('sanitize-filename');
 
 // copied from vscode-extension-tester/src/cli.ts
 function codeStream(stream: string) {
@@ -31,7 +34,7 @@ const runOptions = { // `: RunOptions` , but that is not exported.
 }
 
 async function doit() {
-    // body of `runTests` function in `vscode-extension-tester/src/util/codeUtil.ts`
+    // body of `CodeUtil.runTests` in `vscode-extension-tester/src/util/codeUtil.ts`
     const this_ = (<any>extest).code // escape hatch to access private member
 
     await this_.checkCodeVersion(runOptions.vscodeVersion ?? DEFAULT_RUN_OPTIONS.vscodeVersion)
@@ -48,7 +51,56 @@ async function doit() {
     process.env.TEST_RESOURCES = this_.downloadFolder
     process.env.EXTENSIONS_FOLDER = this_.extensionsFolder
     const runner = new VSRunner(this_.executablePath, literalVersion, this_.parseSettings(runOptions.settings ?? DEFAULT_RUN_OPTIONS.settings), runOptions.cleanup, runOptions.config)
-    return runner.runTests(testFilesPattern, this_, runOptions.logLevel)
+    const code = this_
+    const logLevel = runOptions.logLevel // The logging level for the Webdriver
+    // body of `VSRunner.runTests` in `src/suite/runner.ts`
+    const this__ = (<any>runner) // escape hatch to access private members
+    return new Promise(resolve => {
+        const self = this__
+        const browser: VSBrowser = new VSBrowser(this__.codeVersion, this__.customSettings, logLevel)
+        const universalPattern = testFilesPattern.replace(/'/g, '')
+        const testFiles = glob.sync(universalPattern)
+
+        testFiles.forEach((file) => {
+            if (fs.existsSync(file) && file.substr(-3) === '.js') {
+                this__.mocha.addFile(file)
+            }
+        })
+
+        this__.mocha.suite.afterEach(async function () {
+            if (this__.currentTest && this__.currentTest.state !== 'passed') {
+                try {
+                    const filename = sanitize(this__.currentTest.fullTitle())
+                    await browser.takeScreenshot(filename)
+                } catch (err) {
+                    console.log('Screenshot capture failed.', err)
+                }
+            }
+        })
+
+        this__.mocha.suite.beforeAll(async function () {
+            this.timeout(45000)
+            const start = Date.now()
+            await browser.start(self.chromeBin)
+            await browser.waitForWorkbench()
+            await new Promise((res) => { setTimeout(res, 2000) })
+            console.log(`Browser ready in ${Date.now() - start} ms`)
+            console.log('Launching tests...')
+        })
+
+        this__.mocha.suite.afterAll(async function () {
+            this.timeout(15000)
+            await browser.quit()
+
+            code.uninstallExtension(self.cleanup)
+        })
+
+        this__.mocha.run((failures) => {
+            process.exitCode = failures ? 1 : 0
+            resolve(process.exitCode)
+        })
+    })
+
 }
 
 doit() // top-level await is supported in node in e.g. `.mjs` files, but just wrap it here, because typescript etc.
